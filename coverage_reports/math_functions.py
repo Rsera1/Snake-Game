6eed602c2eeeeca470e29ff1a9320bbe8555c954
Robert Serafin <rsera1@unh.newhaven.edu>
2020-12-05 05:37:46 -0500

    coverage

---
+++ coverage_reports/math_functions.py
@@ -0,0 +1,11 @@
+def addition(a,b):
+	return "{} + {} = {}".format(a,b,a+b)
+
+def subtraction(a,b):
+	return "{} - {} = {}".format(a,b,a-b)
+
+def multiplication(a,b):
+	return "{} * {} = {}".format(a,b,a*b)
+
+def division(a,b):
+	return "{} // {} = {}".format(a,b,a//b)
---
+++ coverage_reports/test_math_functions.py
@@ -0,0 +1,7 @@
+from math_functions import *
+
+def test_math_functions():
+	assert addition(2, 3) == "2 + 3 = 5"
+	assert subtraction(6, 2) == "6 - 2 = 4"
+	assert multiplication(2, 2) == "2 * 2 = 4"
+	assert division(4,2) == "4 // 2 = 2"
